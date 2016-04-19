cbb3472a2685235ab86508b294a81003f03450b7
SolarPolarMan <solarpolarman@gmail.com>
2016-04-19 16:01:01 +0100

    Added more diversity to the aids.

--- jssource.js
+++ jssource.js
@@ -15,18 +15,19 @@ $(document).on("keypress", function (e) {
 });
 
 var intensifiers = ["savage ","gay ","deprived ","desperate ","cringy ",
-"disgusting ","nasty ","hot ","erotic ", "terrifiying ","angry ", "psychotic "]
+"disgusting ","nasty ","hot ","erotic ", "terrifiying ","angry ", "psychotic ",
+"miserable "]
 var ists = ["nastiest ","cringiest ","hottest ","unhappyist ","scariest ",
-"worst ","biggest ","best "]
+"worst ","biggest ","best ","sexiest ","dirtiest ","maddest "]
 var noun = ["minecraft animation ","gay ","man ","kid ","video ","woman ",
 "girl ","manchild ","rapper ","frozen animation ", "pussy slayer ", "youtuber ",
-"music video ","furry ","human being "]
-var placeish = ["on the internet","on youtube","on earth",
+"music video ","furry ","human being ","human ","animation ","girls "]
+var placeish = ["on the internet","on youtube","on earth","planet earth",
 "on the entire internet","to ever touch the internet","to ever be witnessed",
-"to ever live in this world"]
+"to ever live in this world","ever to be created on the internet","to ever live"]
 var cherry = [" roasts me"," wants me"," drinks bleach"," ever",
 " says he is going to kill me"," is in love with me"," (literally eats rocks)",
-" (literally drills themself)"," exposes me"]
+" (literally drills themself)"," exposes me"," ever to be made"," (literally eats bricks)"," "]
 var fontfaces = ["'Ubuntu', sans-serif", "'Dancing Script', cursive"];
 
 function randombool(){
