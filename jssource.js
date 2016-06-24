fb243ab0e55242119319ff5c2cd636a6dd0d6e2a
SolarPolarMan <solarpolarman@gmail.com>
2016-06-24 18:14:26 +0100

    added more filth.

--- jssource.js
+++ jssource.js
@@ -13,27 +13,30 @@ $(document).keypress(function(e) {
 
 var intensifiers = ["savage ","gay ","deprived ","desperate ","cringy ",
 "disgusting ","nasty ","hot ","erotic ", "terrifiying ","angry ", "psychotic ",
-"miserable ","savage cringey ","offensive "];
+"miserable ","savage cringey ","offensive ","insane ","sexually desperate ",
+"offended "];
 
 var ists = ["nastiest ","cringiest ","hottest ","unhappyist ","scariest ",
 "worst ","biggest ","best ","sexiest ","dirtiest ","maddest ","craziest ",
-"saddest ","spookiest ","filthiest "];
+"saddest ","spookiest ","filthiest ","angriest ","strangest "];
 
 var noun = ["minecraft animation ","gay ","man ","kid ","video ","woman ",
 "girl ","manchild ","rapper ","frozen animation ", "pussy slayer ", "youtuber ",
 "music video ","furry ","human being ","human ","animation ","girls ",
-"cyber bully "];
+"cyber bully ","feminist ","emo man "];
 
 var placeish = ["on the internet","on youtube","on earth","on planet earth",
 "on the entire internet","to ever touch the internet","to ever be witnessed",
 "to ever live in this world","ever to be created on the internet","to ever live"
+,"on all of youtube"
 ];
 
 var cherry = [" roasts me"," wants me"," drinks bleach"," ever",
-" says he is going to kill me"," is in love with me"," (literally eats rocks)",
+" says they are going to kill me"," is in love with me"," (literally eats rocks)",
 " (literally drills themself)"," exposes me"," ever to be made",
 " (literally eats bricks)"," (literally breaks into military base)",
-" (literally eats cat hair)","says he is literally going to find and kill me"];
+" (literally eats cat hair)","says they are literally going to find and kill me",
+" tries to have sex with a melon"," rages at me"];
 
 var fontfaces = ["'Roboto', sans-serif", "'Dancing Script', cursive"];
 
