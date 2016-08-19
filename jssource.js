8c76ff4743da13f150dc51d5d9af10f0aa39f131
SolarPolarMan <solarpolarman@gmail.com>
2016-08-19 05:55:58 +0100

    Graphical overhaul. Changed all fonts. Changed colour palette. Did some things.

--- jssource.js
+++ jssource.js
@@ -14,26 +14,29 @@ $(document).keypress(function(e) {
 var intensifiers = ["savage ","gay ","deprived ","desperate ","cringy ",
 "disgusting ","nasty ","hot ","erotic ", "terrifiying ","angry ", "psychotic ",
 "miserable ","savage cringey ","offensive ","insane ","sexually desperate ",
-"offended ","racist ","crazed ","annoying ","motivational ","upset "
+"offended ","racist ","crazed ","annoying ","motivational ","upset ","hardcore "
 ];
 
 var ists = ["nastiest ","cringiest ","hottest ","unhappyist ","scariest ",
 "worst ","biggest ","best ","sexiest ","dirtiest ","maddest ","craziest ",
-"saddest ","spookiest ","filthiest ","angriest ","strangest ","fattest "
+"saddest ","spookiest ","filthiest ","angriest ","strangest ","fattest ",
+"angriest "
 ];
 
 var noun = ["minecraft animation ","gay ","man ","kid ","video ","woman ",
 "girl ","manchild ","rapper ","frozen animation ", "pussy slayer ", "youtuber ",
 "music video ","furry ","human being ","human ","animation ","girls ",
 "cyber bully ","feminist ","emo man ","nazi girl ","feminist chick ",
-"mental breakdown ","youtube girl ","vegan ","diss track ","speech ","emo "
+"mental breakdown ","youtube girl ","vegan ","diss track ","speech ","emo ",
+"racist squad ","vegan girl "
 ];
 
 var placeish = ["on the internet","on youtube","on earth","on planet earth",
 "on the entire internet","to ever touch the internet","to ever be witnessed",
 "to ever live in this world","ever to be created on the internet","to ever live"
 ,"on all of youtube","to exist on youtube ever","on youtube to ever exist",
-"of all time","ever","you'll ever witness on this website","ever to be made"
+"of all time","ever","you'll ever witness on this website","ever to be made",
+"on this entire website"
 ];
 
 var cherry = [" roasts me"," wants me"," drinks bleach"," ever",
@@ -53,10 +56,12 @@ var cherry = [" roasts me"," wants me"," drinks bleach"," ever",
 " literally dumps bucket of cum on head"," (worst rap on all of youtube)",
 " returns mad with fire"," (literally smoking an egg & carrot)",
 " tries to end it all by burning themself in an actual oven"," triggered by me",
-" starts crying"
+" starts crying"," (the cringiest kid ever gets very angry)",
+" (buzzfeed must be stopped now)"," says black people are cows",
+" (the rise of the robots)"
 ];
 
-var fontfaces = ["'Roboto', sans-serif", "'Dancing Script', cursive"];
+var fontfaces = ["'Tiresias', sans-serif","'Lobster', cursive"];
 
 function fontToggle(){
   $('.answer').css('font-family', fontfaces[0]);
